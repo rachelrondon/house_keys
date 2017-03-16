4e3ece956da1d091f156d94fcd7124cb7b4abb8c
Tim Frankie <tfrankie88@gmail.com>
2017-03-16 14:05:12 -0400

    added login jwt on react side

--- src/index.js
+++ src/index.js
@@ -2,28 +2,29 @@ import React from "react";
 import ReactDom from "react-dom";
 import { Route, Router, browserHistory } from "react-router";
 
-// Here we are importing the 8 views
+// Here we are importing the ten views
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 import EditUser from './components/EditUser/EditUser';
 import NewUser from './components/NewUser/NewUser';
 import LandingPage from './components/LandingPage/LandingPage';
 import NewProfile from './components/NewProfile/NewProfile';
-import NewPostRoommate from './components/NewPostRoommate/NewPostRoommate';
+import NewPost from "./components/NewPost/NewPost";
 import NewPostApartment from './components/NewPostApartment/NewPostApartment';
 
 
-// Here are the routes for the 8 views
+// Here are the routes for the ten views
 
 ReactDom.render(
   <Router history={browserHistory}>
     <Route path='/' component={LandingPage} />
     <Route path='/dashboard' component={Dashboard} />
     <Route path='/login' component={Login} />
+    <Route path='/restricted' component={Restricted} />
     <Route path='/user/edit' component={EditUser} />
     <Route path='/user/new' component={NewUser} />
     <Route path='/user/profile' component={NewProfile} />
     <Route path='/user/new/apartment' component={NewPostApartment} />
-    <Route path='/user/new/roommate' component={NewPostRoommate} />
+    <Route path='/user/new/post' component={NewPost} />
   </Router>
   , document.getElementById("app"));
